561bbf96360b9c50fc8cdcc84a0982826cadce04
blackgirlwhocodes <84105697+blackgirlwhocodes@users.noreply.github.com>
2021-12-16 18:34:37 -0500

    Add files via upload

---
+++ loopsexercise.js
@@ -0,0 +1,42 @@
+let counter = 1;
+while (counter <= 10 ){
+    console.log(counter);
+    counter++;
+}
+
+counter = 10;
+while(counter >= 1){
+    console.log(counter);
+    counter--;
+}
+
+for (let i =1; i<= 10 ; i++){
+    console.log(i)
+}
+for (let i =10; i>= 1 ; i--){
+    console.log(i)
+}
+
+counter  = 1;
+do{
+    counter ++;
+    console.log(counter);
+} while (counter <=10)
+
+counter  = 10;
+do{
+    counter --;
+    console.log(counter);
+} while (counter >=1)
+
+counter = 1;
+do{
+    counter++;
+    console.log(counter);
+} while (counter <=10)
+
+counter = 10;
+do{
+    counter++;
+    console.log(counter);
+} while (counter >=1)
